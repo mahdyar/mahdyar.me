50859e7690dd7d784edbc061c372d805ab59cae9
Mahdyar <thisismahdyar@gmail.com>
2020-06-12 00:38:42 +0430

    add 404 page

--- scripts.js
+++ scripts.js
@@ -12,3 +12,21 @@ anime({
     targets: '.link',
     translateY: -30
   });
+
+
+  // Wrap every letter in a span
+var textWrapper = document.querySelector('.ml10 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+  anime.timeline()
+  .add({
+    targets: '.ml10 .letter',
+    rotateY: [-90, 0],
+    duration: 300,
+    delay: (el, i) => 45 * i
+  })
+  .add({
+    targets: '.description',
+    translateY: -30,
+    opacity: 1,
+  });
